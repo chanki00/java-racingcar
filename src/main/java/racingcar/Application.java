19bac4a49be3dbc7e2d7c4792e415e2e60ae0a75
chanki <115077587+chanki00@users.noreply.github.com>
2024-03-31 17:58:23 +0900

    자동차 이름 입력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,10 +3,18 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Application {
+    public static List<String> inputName() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,)로 구분)");
+        List<String> names = Arrays.stream(Console.readLine().split(",")).toList();
+        return names;
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-
+        List<String> cars = inputName();
     }
 }
