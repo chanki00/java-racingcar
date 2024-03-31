a8b91d06fbd91b61887fa1e90115123af47de7fe
chanki <115077587+chanki00@users.noreply.github.com>
2024-03-31 17:55:09 +0900

    처음으로 초기화

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,14 +1,12 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
-import racingcar.controller.RacingController;
-import racingcar.domain.Car;
 
 import java.util.ArrayList;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        RacingController.startRacing();
+
     }
 }
