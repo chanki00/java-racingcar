d878344397dfb3cf9d3d98866b06b39af9d1ada7
chanki <115077587+chanki00@users.noreply.github.com>
2024-03-31 18:46:39 +0900

    자동차 이름 입력
    자동차 이름을 입력 받아 자동차 객체 리스트 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,8 +6,23 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.*;
 
 public class Application {
+    public static List<Car> inputName() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,)로 구분)");
+        String[] names = Console.readLine().split(",");
+        List<Car> cars = new ArrayList<>();
+
+        for (String name : names) {
+            cars.add(new Car(name));
+        }
+
+        return cars;
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        List<Car> cars = inputName();
+
+
 
     }
 }
